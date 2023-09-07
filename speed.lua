53c4c3caa0b436cb831bbf84e9c9b60ef00a9591
Cammy <141085830+mind-set09@users.noreply.github.com>
2023-09-06 22:47:58 -0400

    Create speed.lua

---
+++ speed.lua
@@ -0,0 +1,25 @@
+_G.HackedWalkSpeed = 150 -- change this to your speed that you want to use.
+
+local Plrs = game:GetService("Players")
+
+local MyPlr = Plrs.LocalPlayer
+local MyChar = MyPlr.Character
+
+if MyChar then
+local Hum = MyChar.Humanoid
+Hum.Changed:connect(function()
+Hum.WalkSpeed = _G.HackedWalkSpeed
+end)
+Hum.WalkSpeed = _G.HackedWalkSpeed
+end
+
+
+MyPlr.CharacterAdded:connect(function(Char)
+MyChar = Char
+repeat wait() until Char:FindFirstChild("Humanoid")
+local Hum = Char.Humanoid
+Hum.Changed:connect(function()
+Hum.WalkSpeed = _G.HackedWalkSpeed
+end)
+Hum.WalkSpeed = _G.HackedWalkSpeed
+end)
